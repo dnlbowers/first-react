4dca6a99239ecf8af14e3afb3859c40a8f9e4d15
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-06-17 19:06:00 +0200

    Complete  StatefulGreetingWithPrevState Component

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -12,27 +12,50 @@ class StatefulGreetingWithPrevState extends React.Component {
         };
     }
 
+    // handleClick() {
+    //     this.setState({
+    //         introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+    //         buttonText: this.state.buttonText === "Enter" ? "Exit" : "Enter",
+    //     }, () => {
+    //         // use call back function to await asynconous actions (same as fetch().then())
+    //         console.log('new state', this.state.introduction);
+    //         console.log('new state', this.state.buttonText);
+    //     });
+    //     //here will log the original values of the this.state because setState() is asynchronous
+    //     // console.log(this.state.introduction);
+    //     // console.log(this.state.buttonText);
+    // }
+
+    //Alt way to do handle click when you need to update state based on previous state
     handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: this.state.buttonText === "Enter" ? "Exit" : "Enter",
-        }, () => {
-            // use call back function to await asynconous actions (same as fetch().then())
-            console.log('new state', this.state.introduction);
-            console.log('new state', this.state.buttonText);
-        });
-        //here will log the original values of the this.state because setState() is asynchronous
-        // console.log(this.state.introduction);
-        // console.log(this.state.buttonText);
+        this.setState((prevState, prevProps) =>{
+            console.log('Prev state: ', prevState)
+            console.log('Prev Props: ', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
+        })
     }
 
     incrementCount() {
-        this.setState({
-            count: this.state.count += 1,
-        }, () => {
-            console.log(this.state.count)
+        this.setState((prevState, prevProps) => {
+            console.log('Prev state: ', prevState)
+            console.log('Prev Props: ', prevProps)
+            return {
+                count: prevState.count + 1
+            }
         })
     }
+
+    //my solution had no issue
+    // incrementCount() {
+    //     this.setState({
+    //         count: this.state.count += 1,
+    //     }, () => {
+    //         console.log(this.state.count)
+    //     })
+    // }
     incrementCountFive() {
         this.incrementCount();
         this.incrementCount();
@@ -45,7 +68,8 @@ class StatefulGreetingWithPrevState extends React.Component {
             <>
                 <h1>{this.state.introduction}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-                <button onClick={() => this.incrementCountFive()}>Increase Count</button>
+                <button onClick={() => this.incrementCount()}>Increase Count by 1</button>
+                <button onClick={() => this.incrementCountFive()}>Increase Count by 5</button>
                 <p>You've clicked {this.state.count} times </p>
             </>
         )
