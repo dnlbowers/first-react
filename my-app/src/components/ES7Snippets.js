6d6d3eb08721f075c430b130ddf4de0f56479577
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-06-18 01:34:52 +0200

    add common  es7 snippets file for further reference

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,54 @@
+// not a component just notes on common es7 snipets
+
+//rfce
+// import React from 'react'
+//
+// function Es7Snippets() {
+//     return (
+//         <div>Es7Snippets</div>
+//     )
+// }
+//
+// export default Es7Snippets
+
+// rafce
+// import React from 'react'
+//
+// const Es7Snippets = () => {
+//     return (
+//         <div>Es7Snippets</div>
+//     )
+// }
+// export default Es7Snippets
+
+// rce
+// import React, {Component} from 'react'
+//
+// export class Es7Snippets extends Component {
+//     render() {
+//         return (
+//             <div>Es7Snippets</div>
+//         )
+//     }
+// }
+//
+// export default Es7Snippets
+
+// rconst
+// constructor(props)
+// {
+//     super(props)
+//     this.state = {
+//
+//     }
+// }
+
+//clg
+// console.log()
+
+
+// clo
+// console.log('string', Object )
+
+
+
